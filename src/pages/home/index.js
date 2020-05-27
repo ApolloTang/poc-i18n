ddd2f2c432465cdcddc8e5cc4207e9e1f93c6370
Apollo Tang <tang.apollo@gmail.com>
2020-05-26 21:46:58 -0400

    interpolate variable with FormattedMessage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,11 @@ import {FormattedMessage} from 'react-intl';
 const Home = () => (
   <div>
     <FormattedMessage id="app.content" defaultMessage="Learn React" />
+    <FormattedMessage
+      id="app.channel.plug"
+      defaultMessage="Video brought to you by Coding with Chaim"
+      values={{channelName: 'Coding with Chaim'}}
+    />
   </div>
 );
 
