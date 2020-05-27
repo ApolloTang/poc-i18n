72b7caac7749c78a0512e9461f85d4e11ed43678
Apollo Tang <tang.apollo@gmail.com>
2020-05-26 21:27:46 -0400

    configure tsc to read json; wrap app in IntlProvider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import SimpleNavigation from 'common/components/core/simple-navigation';
+import {IntlProvider} from 'react-intl';
 
 import {Routes, navigationDirective} from './routes';
+import Spanish from '../languages/es-MX.json';
+import English from '../languages/en-US.json';
+
+const local = navigator.language;
+
+let lang = English;
+if (local === 'en-MX') {
+  lang = Spanish
+}
+
 
 const App = () => (
-  <div className="app">
-    <div className="app-navigation-container">
-      <SimpleNavigation navigations={navigationDirective} />
+  <IntlProvider
+    locale={local}
+    messages={lang}
+  >
+    <div className="app">
+      <div className="app-navigation-container">
+        <SimpleNavigation navigations={navigationDirective} />
+      </div>
+      <div className="app-workspace">
+        <Routes />
+      </div>
     </div>
-    <div className="app-workspace">
-      <Routes />
-    </div>
-  </div>
+  </IntlProvider>
 );
 
 export default App;
